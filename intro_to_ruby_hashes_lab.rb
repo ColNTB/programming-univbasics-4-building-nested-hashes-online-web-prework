5cb869036aff821e0d139553147800cc036428d3
Nathan Bohn <nathantbohn@gmail.com>
2019-10-02 11:58:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -63,10 +63,10 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" =>"$100"
 	      },
 	      :b_and_o_railroad => {
-	        "mortgage_value" => "$800"
+	        "mortgage_value" => "$400"
 	      },
 	      :shortline_railroad => {
-	        "mortgage_value" => "$400"
+	        "mortgage_value" => "$800"
 	      },
 	      :pennsylvania_railroad => {
 	        "mortgage_value" => "$200"
