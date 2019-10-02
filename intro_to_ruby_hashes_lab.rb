b563817136bb42926a285bf28a46d428e22c7242
Nathan Bohn <nathantbohn@gmail.com>
2019-10-02 11:56:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,11 +60,17 @@ def monopoly_with_fourth_tier
 	    },
 	    :names => {
 	      :reading_railroad => {
-	        "mortgage"
+	        "mortgage_value" =>"100"
 	      },
-	      :b_and_o_railroad => {},
-	      :shortline_railroad => {},
-	      :pennsylvania_railroad => {}
+	      :b_and_o_railroad => {
+	        "mortgage_value" => "200"
+	      },
+	      :shortline_railroad => {
+	        "mortgage_value" => "400"
+	      },
+	      :pennsylvania_railroad => {
+	        "mortgage_value" => "800"
+	      }
 	    }
 	  }
 	}
